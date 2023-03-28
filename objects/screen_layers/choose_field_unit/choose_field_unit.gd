abb570f6ea0ad6579d1959000cdc1ca1116dcf9a
Apples <2352020+apples@users.noreply.github.com>
2023-03-28 15:56:41 -0500

    Added target arrow

--- objects/screen_layers/choose_field_unit/choose_field_unit.gd
+++ objects/screen_layers/choose_field_unit/choose_field_unit.gd
@@ -4,6 +4,10 @@ signal location_picked(location: ZoneLocation)
 
 var allowed_locations: Array[ZoneLocation] = []
 
+var target_from
+
+@onready var arrow_path = $ArrowPath
+
 func _ready():
 	pass
 
@@ -26,9 +30,23 @@ func uncover():
 		push_warning("No possible targets")
 		emit_signal("location_picked", null)
 		battle_scene.pop_screen()
+	else:
+		results[0].make_current()
+		
+		if target_from is ZoneLocation:
+			var target := battle_scene.get_card_plane(target_from)
+			arrow_path.visible = true
+			arrow_path.start = target.global_position
+		else:
+			arrow_path.visible = false
 
 
 func _on_card_cursor_agent_confirmed(cursor_location):
 	battle_scene.pop_screen()
 	var card_plane: CardPlane = cursor_location.get_parent()
 	emit_signal("location_picked", card_plane.location)
+
+
+func _on_card_cursor_agent_cursor_location_changed(cursor_location):
+	if cursor_location:
+		arrow_path.end = cursor_location.global_position
--- objects/tasks/choose_target.gd
+++ objects/tasks/choose_target.gd
@@ -3,10 +3,15 @@ class_name ChooseTargetTask
 
 var who: ZoneLocation.Side = ZoneLocation.Side.Player
 var allowed_locations: Array[ZoneLocation]
+var source_location: ZoneLocation
 
 func start():
 	var future := Future.new()
-	battle_state.send_message_to(who, MessageTypes.ChooseTarget.new({ allowed_locations = allowed_locations, future = future }))
+	battle_state.send_message_to(who, MessageTypes.ChooseTarget.new({
+		allowed_locations = allowed_locations,
+		source_location = source_location,
+		future = future,
+	}))
 	wait_for_future(future, chosen)
 
 func chosen(where: ZoneLocation):
